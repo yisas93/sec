ec3f63130cbc2636e9f2b13ea7b73b654b31d284
yisas93 <juan249325@gmail.com>
2023-11-15 00:58:50 +0000

    deloy bug

--- securityplus/src/index.js
+++ securityplus/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App/>}/>
+      <Route path="/sec" element={<App/>}/>
     </Routes>
   </BrowserRouter>
 );

422689ac08d51ab63bbdcb35cdff143c6452e006
yisas93 <juan249325@gmail.com>
2023-11-15 01:11:54 +0000

    new router

--- securityplus/src/index.js
+++ securityplus/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter} from "react-router-dom"
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/> 
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/sec" element={<App/>}/>
-    </Routes>
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
